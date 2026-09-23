09b84738d6ffb3ae55817a7d4a817c5ede3d7e96
Akchhat Gurung <24050100623@reva.edu.in>
2026-09-23 15:20:44 +0530

    Add LeetCode Easy - Add Two Integers

---
+++ leetcode/Easy/2383-Add-Two-Integers/solution.java
@@ -0,0 +1,10 @@
+// LeetCode Problem: Add Two Integers
+// Link: https://leetcode.com/problems/add-two-integers/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;    
+    }
+}
